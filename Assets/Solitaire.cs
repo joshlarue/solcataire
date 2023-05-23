36c2995f0b976ebfbb65b9a6447d07d19e66a68a
joshlarue <joshua@laruecrew.ca>
2023-05-23 17:38:53 -0600

    added clicking/tracking click/touch functionality for (hopefully) all platforms

--- Assets/Solitaire.cs
+++ Assets/Solitaire.cs
@@ -12,14 +12,15 @@ public class Solitaire : MonoBehaviour
     public GameObject[] suits;
     public GameObject deckSpot;
     public float offset;
+    public float dealDelay;
 
     // Start is called before the first frame update
     void Start()
     {
-        setupGame();
+        StartCoroutine(setupGame());
     }
 
-    void setupGame()
+    IEnumerator setupGame()
     {
         deck = new List<GameObject>(cards);
 
@@ -37,18 +38,21 @@ public class Solitaire : MonoBehaviour
             for (int j = 0; j <= i; j++)
             {
                 GameObject card = Instantiate(deck[cardIndex], stacks[i].transform.position - new Vector3(0, j * offset, j * offset), Quaternion.identity);
+                
                 cardIndex++;
 
                 if (j == i)
                 {
                     card.GetComponent<Card>().FlipFaceUp();
                 }
+                yield return new WaitForSeconds(dealDelay);
             }
         }
 
         for (int i = cardIndex; i < deck.Count; i++)
         {
             GameObject card = Instantiate(deck[i], deckSpot.transform);
+            yield return new WaitForSeconds(dealDelay);
         }
 
         RemoveOriginalCards();
@@ -65,6 +69,41 @@ public class Solitaire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        #if UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            RaycastHit2d hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Card hitCard = hit.collider.GetComponent<Card>();
+                if (hitCard != null)
+                {
+                    cardInteractionLogic();
+                    Debug.Log("touched card: " + hitCard.name);
+                }
+            }
+        }
+        #else
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Card hitCard = hit.collider.GetComponent<Card>();
+                if (hitCard != null)
+                {
+                    cardInteractionLogic();
+                    Debug.Log("clicked card: " + hitCard.suit + hitCard.rank);
+                }
+            }
+        }
+        #endif
+    }
+
+    void cardInteractionLogic()
+    {
+
     }
 }
